99a4882827038822877e4df5883b4a5259ad06f1
Diego Garzon <digarodev@gmail.com>
2020-07-19 19:24:37 -0500

    add the task 0, define the method canUnlockAll

---
+++ 0x00-lockboxes/0-lockboxes.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+'''
+    Method:
+        canUnlockAll(boxes) - Method that check if all the boxes can be opened.
+    Parameters:
+        boxes: list of list
+'''
+
+
+def canUnlockAll(boxes):
+    result = False
+    if boxes:
+        checklist = [False] * len(boxes)
+        pending = list()
+
+        checklist[0] = True
+
+        for box in boxes:
+            if checklist[boxes.index(box)]:
+                for key in box:
+                    checklist[key] = True
+                    if key in pending:
+                        for value in boxes[key]:
+                            if not checklist[value]:
+                                checklist[value] = True
+                                pending.remove(key)
+            else:
+                pending.append(boxes.index(box))
+
+        for check in checklist:
+            if check:
+                result = True
+            else:
+                result = False
+                break
+
+    return result
