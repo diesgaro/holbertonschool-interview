3ae19f2a016cc8f65e65d522ac31ec5a00ae5dcf
Diego Garzon <digarodev@gmail.com>
2020-09-23 22:16:55 -0500

    task 0

---
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+UTF-8 Validation: method that determines if a given data
+set represents a valid UTF-8 encoding.
+"""
+
+
+def validUTF8(data):
+    n_bytes = 0
+    bit_1 = 1 << 7
+    bit_2 = 1 << 6
+
+    for num in data:
+        bit = 1 << 7
+        if n_bytes == 0:
+            while bit & num:
+                n_bytes += 1
+                bit = bit >> 1
+
+            if n_bytes == 0:
+                continue
+
+            if n_bytes == 1 or n_bytes > 4:
+                return False
+        else:
+            if not (num & bit_1 and not (num & bit_2)):
+                return False
+            n_bytes -= 1
+
+    if n_bytes == 0:
+        return True
+    else:
+        return False
